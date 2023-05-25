f91e4a36e4201a76d0bb7699a5df6ab2ca0d05d3
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-05-25 17:49:14 +0000

    discussion on variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "Nadeem2200@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 Not Allowed
+accountEmail = "fahad@gmail.com"
+accountPassword = "23456"
+accountCity = "Delhi"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
