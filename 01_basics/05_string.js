b755a3971e3016b3ba6c149096d5552135f36efd
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-05-28 17:57:39 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,29 @@
+const Tanda= "Fahad"
+const repoCount = 50
+
+// console.log(`Hello my name is ${Tanda} my repo count is ${repoCount}`);
+
+const gameName = new String('NadeemFahad')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('3'));
+// console.log(gameName.indexOf('d'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-10,3)
+// console.log(anotherString);
+
+const newStringone = "    Fahad    "
+console.log(newStringone);
+console.log(newStringone.trim());
+
+
+const url = "https://Nadeem.com/nadeem%20fahad"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('Nadeem'));
+
+
