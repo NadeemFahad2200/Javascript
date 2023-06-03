2e8dbddb60e77ee1f5baa987d2085f214008cda8
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-06-03 19:20:18 +0000

    json talk

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,51 @@
+// const tinderUser = new Object()    SingleTone object Declaration
+// const tinderUser = {} Non Singleton object Declaration
+const tinderUser = {}
+tinderUser.id = "123abd"
+tinderUser.name = "Nadeem" 
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+const regularUser = {
+    email:  "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Nadeem",
+            lastname: "Fahad"
+
+        }
+
+    }
+}
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = Object.assign({}, obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+const users =[
+
+{
+
+},
+{
+
+},
+{
+
+},
+]
+
+
+users[1].email
+// console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
