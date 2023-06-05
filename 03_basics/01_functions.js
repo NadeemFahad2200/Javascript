bbf3f4e8e9f25813aaf23c9f8e68e78efbad3bf2
Mohd Nadeem <102049855+NadeemFahad2200@users.noreply.github.com>
2023-06-05 18:22:07 +0000

    taking parameters in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,7 +26,7 @@ const result = addTwoNumbers(3,5)
 
 function loginuserMessage(username = "Nadeem"){
     if(username === undefined){
-        console.log("Please enter a username");
+        // console.log("Please enter a username");
         return
 
     }
@@ -34,4 +34,31 @@ function loginuserMessage(username = "Nadeem"){
     
 }
 // console.log(loginuserMessage("Nadeem"))
-console.log(loginuserMessage("Aisha"))
+// console.log(loginuserMessage("Aisha"))
+function calculatecartprice(var1, var2, ...num1) {
+    return num1
+}
+// console.log(calculatecartprice(12, 222, 432, 345)) 
+
+const user = {
+    username: "Nadeem",
+    price: 1999
+}
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+// handleobject(user)
+handleobject({
+    username: "Sam",
+    price:1987
+})
+
+
+const mynewarray = [200, 400, 500, 800]
+function returnsecondvalues(getarray) {
+    return getarray[1]
+    
+}
+// console.log(returnsecondvalues(mynewarray));
+console.log(returnsecondvalues([200, 400, 500, 800]));
